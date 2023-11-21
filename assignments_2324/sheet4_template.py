db90f1046601b45a0a278ae60d61203d3702fd0e
Freya Bretz <freya.bretz@uni-heidelberg.de>
2023-11-21 12:10:06 +0100

    New Problem sheets

---
+++ assignments_2324/sheet4_template.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def COMPUTE_DOMINANT_EIGENVALUE(A):
+
+def COMPUTE_EIGENVALUES(A):
+		
+		
+if __name__ == "__main__":
+    A = np.array([[1+1j,1,0,0,0],[0,1+1j,1,0,0],[0,0,1+1j,1,0],[0,0,0,1+1j,1],[1,0,0,0,1+1j]])
+    print("Input test matrix:")
+    print(A)
+    
+    eigenvalues = COMPUTE_EIGENVALUES(A)
+    print("Eigenvalues of A:")
+    print(eigenvalues)	 
+	
